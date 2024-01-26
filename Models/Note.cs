1bb5a4902fe14d36a552f2c205d4f99a4bd42f1c
Lee1090 <sleeyer1090@gmail.com>
2024-01-26 17:08:31 +1300

    Create NoteBook and Note models

---
+++ Models/Note.cs
@@ -0,0 +1,16 @@
+﻿using EasyXNoteDataAccessAPI.Models;
+using System;
+
+namespace EasyXNoteDataAccessAPI.Models
+{
+    public class Note
+    {
+        public int NoteID { get; set; }
+        public int UserID { get; set; }
+        public int NoteBookID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
---
+++ Models/NoteBook.cs
@@ -0,0 +1,14 @@
+﻿using EasyXNoteDataAccessAPI.Models;
+using System;
+
+namespace EasyXNoteDataAccessAPI.Models
+{
+    public class NoteBook
+    {
+        public int NoteBookID { get; set; }
+        public int UserID { get; set; }
+        public string NoteBookName { get; set; }
+        public bool IsDefault { get; set; }
+        public int SortOrder { get; set; }
+    }
+}
