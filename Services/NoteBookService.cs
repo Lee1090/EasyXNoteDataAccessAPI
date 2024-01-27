4742638c9c2a3b2ce1e52f1637e0f0eb348486ff
Lee1090 <sleeyer1090@gmail.com>
2024-01-27 17:21:37 +1300

    Create NoteBookService and NoteService classes

---
+++ Services/NoteBookService.cs
@@ -0,0 +1,24 @@
+﻿using EasyXNoteDataAccessAPI.Data;
+using EasyXNoteDataAccessAPI.Models;
+using System;
+using System.Collections.Generic;
+
+namespace EasyXNoteDataAccessAPI.Services
+{
+    public class NoteBookService
+    {
+        private readonly NoteBookRepository noteBookRepository;
+
+        public NoteBookService()
+        {
+            noteBookRepository = new NoteBookRepository();
+        }
+
+        public object GetAllNoteBooks()
+        {
+            return noteBookRepository.GetAllNoteBooks();
+        }
+    }
+
+}
+
---
+++ Services/NoteService.cs
@@ -0,0 +1,24 @@
+﻿using EasyXNoteDataAccessAPI.Data;
+using EasyXNoteDataAccessAPI.Models;
+using System;
+using System.Collections.Generic;
+
+namespace EasyXNoteDataAccessAPI.Services
+{
+    public class NoteService
+    {
+        private readonly NoteRepository noteRepository;
+
+        public NoteService()
+        {
+            noteRepository = new NoteRepository();
+        }
+
+        public object GetAllNotes()
+        {
+            return noteRepository.GetAllNotes();
+        }
+    }
+
+}
+
